4983e4d45356b527b0ba2c763e6c3d093e445aaf
Khoerul Umam <43531752+sahmura@users.noreply.github.com>
2019-01-30 22:26:55 +0700

    Delete connectdb.php

--- connectdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbhost = 'localhost';
-$dbusername = 'id5361828_ilkomunnes';
-$dbpassword = 'khoerul27umam';
-$dbname = 'id5361828_ilkomunnes';
-
-$dbconn = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
